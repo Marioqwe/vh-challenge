6a2d94f4d07ac47d927d24c523e74d1570a68a2d
Mario Solano <mario.solanogonzales@MarioSolanoGonzalesMac.local>
2020-05-16 10:38:14 -0400

    adds getSampleQuestionText static method

--- app/Question.php
+++ app/Question.php
@@ -14,4 +14,14 @@ class Question extends Model
         return $this->hasMany(Answer::class);
     }
 
+    static public function getSampleQuestion() {
+        $SAMPLE_QUESTIONS = [
+            'Isn’t it hard to be vegan?',
+            'Isn’t it expensive to be vegan?',
+            'Can you be an athlete on a vegan diet?',
+        ];
+
+        $k = array_rand($SAMPLE_QUESTIONS );
+        return $SAMPLE_QUESTIONS[$k];
+    }
 }
