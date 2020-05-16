6174cefb9f359b421c2515f408948bb51e03e8be
Mario Solano <mario.solanogonzales@MarioSolanoGonzalesMac.local>
2020-05-16 12:41:12 -0400

    add bootstrap

--- resources/views/question_with_id.blade.php
+++ resources/views/question_with_id.blade.php
@@ -4,41 +4,59 @@
 
 @section('content')
 
-    <a href="/">Questions</a>
+    <a href="/">Show me all questions</a>
+    <hr />
+
+    <h2 class="text-center mb-4">{{ $question->text }}</h2>
 
     <br />
-    <br />
 
-    {{ $question->text }}
+    <div class="container mb-5">
+        @forelse($answers as $answer)
+            <div class="row">
+                <div class="col mb-2">
+                    <div class="card">
+                        <div class="card-body">
+                            {{ $answer->text }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @empty
+            <div class="row">
+                <div class="col mb-2">
+                    <div class="card">
+                        <div class="card-body">
+                            No answers yet! Be the first to answer by using the form below.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforelse
+    </div>
 
-    <br />
+    <hr />
 
-    <ul>
-        @foreach($answers as $answer)
-            <li> {{ $answer->text }} </li>
-        @endforeach
-    </ul>
-
-    <br />
-    <br />
-
-    Answer the question:
+    <h2 class="text-center mb-4">Answer the Question</h2>
 
     <form action="/questions/{{ $question->id }}/answer" method="post">
         {{ csrf_field() }}
         <input type="hidden" name="question_id" value="{{ $question->id }}">
-        <input type="text" name="text" value="{{ old('text') }}" placeholder="Enter you answer">
-        <button type="submit">Submit</button>
-    </form>
-
-    @if ($errors->any())
-        <div>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+        <div class="form-group">
+            <textarea class="form-control {{ $errors->any() ? 'is-invalid' : '' }}" name="text" placeholder="Enter your answer">{{ old('text') }}</textarea>
+            <div class="invalid-feedback">
+                @if ($errors->any())
+                    @foreach ($errors->all() as $error)
+                        @if ($loop->first)
+                            {{ $error }}
+                        @endif
+                    @endforeach
+                @endif
+            </div>
         </div>
-    @endif
+        <div class="text-right">
+            <button type="submit" class="btn btn-primary mb-2">Submit answer</button>
+        </div>
+    </form>
 
 @endsection
--- resources/views/questions.blade.php
+++ resources/views/questions.blade.php
@@ -4,37 +4,53 @@
 
 @section('content')
 
-    Ask a question:
-
     <form action="/questions" method="post">
         {{ csrf_field() }}
-        <input type="text" name="text" value="{{ old('text') }}" placeholder="{{ $sampleQuestion }}">
-        <button type="submit">Submit</button>
+        <div class="form-group">
+            <textarea class="form-control {{ $errors->any() ? 'is-invalid' : '' }}" name="text" placeholder="{{ $sampleQuestion }}">{{ old('text') }}</textarea>
+            <div class="invalid-feedback">
+                @if ($errors->any())
+                    @foreach ($errors->all() as $error)
+                        @if ($loop->first)
+                            {{ $error }}
+                        @endif
+                    @endforeach
+                @endif
+            </div>
+        </div>
+        <div class="text-right">
+            <button type="submit" class="btn btn-primary mb-2">Ask Away</button>
+        </div>
     </form>
 
-    @if ($errors->any())
-        <div>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+    <hr/>
 
-    <br />
-    <br />
-
-    Questions
-
-    <br />
-
-    <ul>
-        @foreach($questions as $question)
-            <li>
-                <a href="/questions/{{ $question->id }}">{{ $question->text }}</a>  <span>{{ $question->answers_count }}</span>
-            </li>
-        @endforeach
-    </ul>
+    <h2 class="text-center mb-4">Questions</h2>
+    <div class="container">
+        @forelse($questions as $question)
+            <div class="row">
+                <div class="col mb-2">
+                    <div class="card">
+                        <div class="card-body d-flex justify-content-between">
+                            <a class="pr-4 text-dark" href="/questions/{{ $question->id }}">{{ $question->text }}</a>
+                            <div class="d-flex flex-column justify-content-center">
+                                <span class="badge badge-primary self-align-center">{{ $question->answers_count }} answers</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @empty
+            <div class="row">
+                <div class="col mb-2">
+                    <div class="card">
+                        <div class="card-body">
+                            No questions yet! Be the first to ask a question by using the form above.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforelse
+    </div>
 
 @endsection
