f53435bcf06be208269e71dc3f2afea1417861db
Mario Solano <mario.solanogonzales@MarioSolanoGonzalesMac.local>
2020-05-16 09:49:16 -0400

    remove dead code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,15 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Question;
 
 class HomeController extends Controller
 {
 
     public function index() {
-//        $questions = Question::all();
         $questions = Question::withCount(['answers'])->get();
         return view('home', [
             'questions' => $questions,
